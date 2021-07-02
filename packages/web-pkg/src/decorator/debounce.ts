d16744ec7c304aec0e7355eb4d686c7903835d8d
Florian Schade <f.schade@icloud.com>
2021-07-02 11:56:36 +0200

    add decorator tests

--- packages/web-pkg/src/decorator/debounce.ts
+++ packages/web-pkg/src/decorator/debounce.ts
@@ -11,12 +11,13 @@ export default (
   return function(target: unknown, key: string, descriptor: PropertyDescriptor): void {
     const fn = descriptor.value
     const bouncer = debounce(
-      function(
+      async function(
         executor: { resolve: (value: any) => void; reject: (reason: any) => void },
         ...args: any[]
       ) {
         try {
-          executor.resolve(fn.apply(this, args))
+          const result = await fn.apply(this, args)
+          executor.resolve(result)
         } catch (error) {
           executor.reject(error)
         }
